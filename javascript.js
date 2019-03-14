b73eda51c7e6156808071ef1c82a657f2644391a
ralitsaJS <48558237+ralitsaJS@users.noreply.github.com>
2019-03-14 15:50:19 +0200

    Add files via upload
    Initial commit

---
+++ javascript.js
@@ -0,0 +1,20 @@
+/*
+let user={
+	firstName : "Pesho",
+	lestName : "Petrov",
+	town: "Sofia",
+	postCode:"1504"
+}
+*/
+
+
+let user={
+	firstName : "Pesho",
+	lestName : "Petrov",
+	addres:{
+		town: "Sofia",
+		postCode:1504,
+	}
+}
+user1.addres.country = "Bulgaria";
+console.log(user.firstName);
